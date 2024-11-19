759df4ba18f13e437c06e8237ce1772f1dd7fded
immy-sleepless <napath3467@gmail.com>
2024-11-19 22:09:37 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1hRuzkSICyJ3tq7mpgBfChgSjLH6yTS0b
 """
 
-pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import numpy as np
